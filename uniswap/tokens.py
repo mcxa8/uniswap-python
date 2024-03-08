a9739fb41eb7edb6ead7d5502fde3aff0c0e099b
mcxa8 <161068872+mcxa8@users.noreply.github.com>
2024-03-08 12:43:44 +0800

    Update tokens.py

--- uniswap/tokens.py
+++ uniswap/tokens.py
@@ -16,6 +16,14 @@ tokens_mainnet: Dict[str, ChecksumAddress] = {
     }.items()
 }
 
+//"ETH": "0x0000000000000000000000000000000000000000",
+ //       "WETH": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+//        "DAI": "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+//        "BAT": "0x0D8775F648430679A709E98d2b0Cb6250d2887EF",
+ //       "WBTC": "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
+ //       "UNI": "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",
+ //       "USDC": "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+
 tokens_rinkeby: Dict[str, ChecksumAddress] = {
     k: Web3.to_checksum_address(v)
     for k, v in {
